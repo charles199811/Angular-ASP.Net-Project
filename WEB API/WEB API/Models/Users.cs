c0fdc2b83d7b406a00ce34ddf8da340ba31a8f4d
charles199811 <charlesrodrio@gmail.com>
2022-10-02 16:08:32 +0530

    angular with asp.net

---
+++ WEB API/WEB API/Models/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB_API.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        
+
+    }
+}
